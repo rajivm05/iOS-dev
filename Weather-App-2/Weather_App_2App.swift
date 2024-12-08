6b2d6688b4563841883d69d70eba6e6622a1fffb
Rajiv Murali <rajivmurali2000@gmail.com>
2024-12-08 11:25:50 -0800

    Initial Commit

---
+++ Weather-App-2/Weather_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_2App.swift
+//  Weather-App-2
+//
+//  Created by Rajiv Murali on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-App-2Tests/Weather_App_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_2Tests.swift
+//  Weather-App-2Tests
+//
+//  Created by Rajiv Murali on 12/8/24.
+//
+
+import Testing
+@testable import Weather_App_2
+
+struct Weather_App_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
